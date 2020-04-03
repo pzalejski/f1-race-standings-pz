4791401e59bbf32c026a25ae7e60470b7e6fa86c
pzalejski <piotrzalejski@live.com>
2020-04-03 02:19:27 -0500

    updated form to show table once year and lap are provided, get standings button onclick will display table with the top 7 Formula 1 drivers (name,nationality,sponsor,points) - pz

--- js/main.js
+++ js/main.js
@@ -1,29 +1,31 @@
 
+function submitForm() {
+    let i = 0;
+    let year = ($('#year_lap input[name=year]').val())
+    let lap = ($('#year_lap input[name=lap]').val())
+    $('.table').attr('id','showTable')
+    $.get("https://ergast.com/api/f1/" + year + "/" + lap + "/driverStandings.json", function (data) {
+        while (i < 8) {
+            //  grabbing data with drivers first name testing via console
+            console.log(data.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].Driver.familyName)
 
+            // display drivers first and last name in table
+            $(`.${i} .name`).html((data.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].Driver.givenName) + ' ' + (data.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].Driver.familyName));
 
+            // displayinbg driver's nationality in table
+            $(`.${i} .nationality`).html(data.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].Driver.nationality);
 
-function submitForm(){
-        let year = ($('#year_lap input[name=year]').val())
-        let lap = ($('#year_lap input[name=lap]').val())
-        $.get("https://ergast.com/api/f1/" + year + "/" + lap + "/driverStandings.json", function (data) {
-            //  grabbing data with drivers first name
-            document.getElementById('name)')
-            console.log(data.MRData.StandingsTable.StandingsLists[0].DriverStandings[0].Driver.givenName)
-
-        //     // grabbing data with drivers last name
-        //     console.log(data.MRData.StandingsTable.StandingsLists[0].DriverStandings[0].Driver.familyName)
-
-        //     // grabbing driver's nationality
-        //     console.log(data.MRData.StandingsTable.StandingsLists[0].DriverStandings[0].Driver.nationality)
-
-        //     //grabbing drivers sponsor
-        //     console.log(data.MRData.StandingsTable.StandingsLists[0].DriverStandings[0].Constructors[0].constructorId)
+            //display drivers sponsor in table
+            $(`.${i} .sponsor`).html(data.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].Constructors[0].constructorId);
+        
+            //display drivers points in table
+            $(`.${i} .points`).html(data.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].points);
             
-        //     //grabbing drivers sponsor
-        //     console.log(data.MRData.StandingsTable.StandingsLists[0].DriverStandings[0].Constructors[0].constructorId)
-            // 
+            
+            i++
+        }
         })
-    }
+}
 
 
 
